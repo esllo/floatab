f84b76a8c1cc0d1f8a941b8a9f9147ae6cc4d6d1
esllo <master@esllo.com>
2023-02-15 01:00:36 +0900

    feat(rust): insert_script, cleanup_windows 함수 추가

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,45 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
+#[tauri::command]
+fn insert_script(
+    app_handle: tauri::AppHandle,
+    label: String,
+    script: String,
+) -> Result<bool, bool> {
+    let window = app_handle.get_window(&label);
+
+    match window {
+        Some(_) => match window.unwrap().eval(&script) {
+            Ok(_) => Ok(true),
+            Err(_) => Err(false),
+        },
+        None => Err(false),
+    }
+}
+
+#[tauri::command]
+fn cleanup_windows(app_handle: tauri::AppHandle, labels: Vec<String>) {
+    let windows = app_handle.windows();
+
+    for (_, window) in &windows {
+        let label = window.label();
+        if label == "main" {
+            continue;
+        }
+        if !labels.contains(&label.to_string()) {
+            match window.close() {
+                _ => (),
+            }
+        }
+    }
+}
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![cleanup_windows, insert_script])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
